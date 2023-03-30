d575045fb578e1a3523f98b24593638fa336a402
Anton Balashov <tyquat@gmail.com>
2023-03-30 13:48:47 +0300

    Task 4 corrected

--- Program.cs
+++ Program.cs
@@ -8,12 +8,15 @@ int a = int.Parse(Console.ReadLine());
 int b = int.Parse(Console.ReadLine());
 int c = int.Parse(Console.ReadLine());
 
-if(a >= b & a >= c){
+if(a > b & b >= c){
     Console.WriteLine($"max = {a}");
 }
-if(b >= a & b >= c){
+if(b > a & a >= c){
     Console.WriteLine($"max = {b}");
 }
-if(c >= a & c >= b){
+if(c > b & b >= c){
     Console.WriteLine($"max = {c}");
+}
+if (a == b & b == c){
+    Console.WriteLine("Everything is equal");
 }
